34e23a83abbc77ca79fb95a724c9d7be15f5c1e3
Phuc Tran <tqp1610@gmail.com>
2025-11-28 16:04:20 +0700

    Add card component definition to script.js

--- interactive-3d-cards/script.js
+++ interactive-3d-cards/script.js
@@ -1,1 +1,19 @@
 Vue.config.devtools = true;
+
+Vue.component('card', {
+    template: `
+        <div class="card-wrap"
+            @mousemove="handleMouseMove"
+            @mouseenter="handleMouseEnter"
+            @mouseleave="handleMouseLeave"
+            ref="card">
+            <div class="card"
+                :style="cardStyle">
+                <div class="card-bg" :style="[cardBgTransform, cardBgImage]"></div>
+                <div class="card-info">
+                <slot name="header"></slot>
+                <slot name="content"></slot>
+                </div>
+            </div>
+        </div>`,
+})
